3ed419f063a3634f46df881c62498ac428dcba81
xfans <zhuleiv5@outlook.com>
2017-09-26 00:03:32 +0800

    change svg loader

--- app/src/main/java/com/examples/demo/SVGTestActivity.java
+++ app/src/main/java/com/examples/demo/SVGTestActivity.java
@@ -3,19 +3,29 @@ package com.examples.demo;
 import android.app.Activity;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.PictureDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.graphics.drawable.DrawableCompat;
 import android.view.Gravity;
 import android.view.View;
 
+import com.bumptech.glide.GenericRequestBuilder;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.model.StreamEncoder;
+import com.bumptech.glide.load.resource.file.FileToStreamDecoder;
 import com.caverock.androidsvg.SVG;
 import com.caverock.androidsvg.SVGAndroidRenderer;
 import com.caverock.androidsvg.SVGParseException;
 import com.examples.demo.utils.GroupXY;
 import com.examples.demo.utils.Marker;
+import com.examples.demo.utils.SvgDecoder;
+import com.examples.demo.utils.SvgDrawableTranscoder;
+import com.examples.demo.utils.SvgSoftwareLayerSetter;
 import com.examples.demo.widget.MarkerGestureImageView;
 
+import java.io.InputStream;
 import java.util.List;
 
 
@@ -27,40 +37,62 @@ import java.util.List;
 public class SVGTestActivity extends Activity {
 
     private MarkerGestureImageView imageView;
-
+    private GenericRequestBuilder<Uri, InputStream, SVG, PictureDrawable> requestBuilder;
+    Drawable icon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         imageView = new MarkerGestureImageView(this);
         imageView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-        try
-        {
-            SVG svg = SVG.getFromResource(this, R.raw.svg_1_g);
-            Drawable drawable = new PictureDrawable(svg.renderToPicture());
-            imageView.setImageDrawable(drawable);
-        }
-        catch(SVGParseException e)
-        {}
-        imageView.getController().getSettings().setMaxZoom(4f);
 
+        requestBuilder = Glide.with(this)
+                .using(Glide.buildStreamModelLoader(Uri.class, this), InputStream.class)
+                .from(Uri.class)
+                .as(SVG.class)
+                .transcode(new SvgDrawableTranscoder(), PictureDrawable.class)
+                .sourceEncoder(new StreamEncoder())
+                .cacheDecoder(new FileToStreamDecoder<SVG>(new SvgDecoder()))
+                .decoder(new SvgDecoder())
+                .skipMemoryCache(true)
+                .animate(android.R.anim.fade_in)
+                .listener(new SvgSoftwareLayerSetter<Uri>(new SVGListener() {
+                    @Override
+                    public void success() {
+
+                        List<GroupXY> groupXYList = SVGAndroidRenderer.getGroupXYList();
+                        for (GroupXY g:groupXYList){
+                            imageView.addMarker(new Marker()
+                                    .setIcon(icon)
+                                    .setLocation(g.getX(), g.getY()) // Novosibirsk
+                                    .setGravity(Gravity.CENTER)
+                            );
+                        }
+                    }
+                }));
+
+
+//        try
+//        {
+//            SVG svg = SVG.getFromResource(this, R.raw.svg_1_g);
+//            Drawable drawable = new PictureDrawable(svg.renderToPicture());
+//            imageView.setImageDrawable(drawable);
+//        }
+//        catch(SVGParseException e)
+//        {}
+        imageView.getController().getSettings().setMaxZoom(4f);
+//
         setContentView(imageView);
-        List<GroupXY> groupXYList = SVGAndroidRenderer.getGroupXYList();
-        // Getting tinted icon drawable
-        Drawable icon = ContextCompat.getDrawable(this, R.drawable.ic_circle_hollow);
+        loadNet();
+
+//        // Getting tinted icon drawable
+        icon = ContextCompat.getDrawable(this, R.drawable.ic_circle_hollow);
         icon = DrawableCompat.wrap(icon);
         DrawableCompat.setTint(icon, ContextCompat.getColor(this, R.color.colorAccent));
+//
+//        final float density = getResources().getDisplayMetrics().density;
+//        final int iconTipOffsetY = Math.round(4 * density); // Pin's tip vertical offset
 
-        final float density = getResources().getDisplayMetrics().density;
-        final int iconTipOffsetY = Math.round(4 * density); // Pin's tip vertical offset
-        for (GroupXY g:groupXYList){
-            imageView.addMarker(new Marker()
-                    .setIcon(icon)
-                    .setLocation(g.getX(), g.getY()) // Novosibirsk
-                    .setGravity(Gravity.CENTER)
-                    .setOffset(0, iconTipOffsetY) // Icon's focal point (pin tip) offset
-            );
-        }
 //        imageView.addMarker(new Marker()
 //                .setIcon(icon)
 //                .setLocation(917, 119) // Novosibirsk
@@ -85,5 +117,16 @@ public class SVGTestActivity extends Activity {
     //protected void onSettingsChanged() {
         //getSettingsListener().onSetupGestureView(imageView);
     //}
+    private void loadNet() {
+        Uri uri = Uri.parse("https://raw.githubusercontent.com/xfans/androidexamples/master/app/src/main/res/raw/svg_1_g.svg");
+        requestBuilder
+                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                // SVG cannot be serialized so it's not worth to cache it
+                .load(uri)
+                .into(imageView);
+    }
 
+    public interface SVGListener{
+        void success();
+    }
 }
